8da53f53cc1e87d3c2025a9fca0741c5c844c7b7
bala-amypo <bala@amypo.in>
2025-12-18 10:11:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -2,22 +2,23 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.example.demo.entity.ValidationEntity;
 import com.example.demo.service.ValidationService;
-import java.util.List;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import java.util.List;
 import jakarta.validation.Valid;
-
 
 @RestController
 public class ValidationController{
     @Autowired ValidationService ser;
 @PostMapping("/postt")
-public ValidationEntity dopost(@Valid @RequestBody ValidationEntity val){
-    return ser.postdataa(val);
+public ValidationEntity dopost( @Valid @RequestBody ValidationEntity stuu){
+    return ser.postdata(stuu);
+
 }
+
 }
