815c893e6d127809f2caf8aa0caa1810e19a7229
bala-amypo <bala@amypo.in>
2025-12-19 09:36:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/StudentDetails.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity ;
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer regNo;
+    private String name;
+    private String address;
+    } 
